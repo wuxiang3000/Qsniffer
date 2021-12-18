3d2d6015a91e83b6f876a812f4cbfe472bc0d7e1
Hsiang Woo <wuxiang3000@outlook.com>
2021-12-19 01:05:34 +0800

    update UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,25 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include "capture_dialog.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //ui->tabWidget->addTab(new capture_dialog(), "Capture");
+    //ui->tabWidget->addTab(new QWidget(), "Configuration");
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+     //ui->listWidget->addItem("Netgear");
+    //ui->tableWidget->row
 }
 
